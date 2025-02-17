5932d49696e4bd17535804d960daf8280fd26ae0
shukumar-542 <shukumar542@gmail.com>
2025-02-17 18:03:55 +0600

    Rest operator

--- restOperator.js
+++ restOperator.js
@@ -1,1 +1,15 @@
-const arr = [1,2,3,4]
+
+
+const arr = [1,2,3,4,5]
+
+// const first  = arr[0]
+const rest  = arr.slice(1)
+// console.log(rest);
+
+
+// after the rest operator
+const [first , ...rest1] = arr;
+
+console.log(first);
+console.log("rest" , rest1);
+ 
