e73dc855a7aad55ef0596a3a63a7c00fa8253769
shukumar-542 <shukumar542@gmail.com>
2025-04-04 12:12:22 +0600

    Learn clouser

--- closure.js
+++ closure.js
@@ -1,22 +1,45 @@
 /**
  * Closures function occurs when inner function remember the variables from it's outer function.
  * even outer function has finished
- * 
+ * Closure allow to us to create private variable that can not  be access directly
+ * Closure allow to us generate multiple function dynamically
+ *  
  */
 
 //=== First example for the closure
 
-function counter (){
-    let count  = 0;
+function incrementDecrement() {
+  let count = 0;
 
-    return ()=>{
-        count++;
-        console.log(count);
-
-    }
+  return {
+    increment: () => {
+      count++;
+      console.log(count);
+    },
+    decrement: () => {
+      count--;
+      console.log(count);
+    },
+  };
 }
 
-const increment = counter()
-increment()
-increment()
+const counter = incrementDecrement();
+// counter.increment()
+// counter.increment()
+// counter.decrement()
+// increment()
+// increment()
 // console.log(increment());
+
+// ==== Second example 
+// closure allow to us generate multiple function dynamically
+
+function greeting (message){
+    
+    return (name)=>{
+        console.log(message , name);
+    }
+}
+const hello = greeting("Hi");
+
+hello("Shukumar")
