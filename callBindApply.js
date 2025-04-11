df62cdf27f7e074b2af96d39c3a166d5bab49e56
shukumar-542 <shukumar542@gmail.com>
2025-04-11 22:30:14 +0600

    Call method

--- callBindApply.js
+++ callBindApply.js
@@ -14,5 +14,25 @@ const greeting ={
     }
 }
 
-greeting.sayHello();
+// greeting.sayHello();
+
+
+// Explicit binding
+// call parameter ==============
+// 1st --- object 
+// 2nd and more ---- variable
+
+const printName = function ( address , phone){
+    console.log(this.name , this.city,` ${address},  ${phone}`)
+}
+
+const person ={
+    name : "Shukumar Ghosh",
+    city : "Dhaka"
+}
+
+const phone = "01872999038"
+const address = "Jhenaidah"
+printName.call(person , address , phone)
+
 
