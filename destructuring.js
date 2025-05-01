12ab3e0120ae409ae251791f69ab42f0e8b7eecf
shukumar-542 <shukumar542@gmail.com>
2025-05-01 12:01:02 +0600

    object destructuring

---
+++ destructuring.js
@@ -0,0 +1,46 @@
+const user = {
+    name: "Shukumar",
+    age: 25,
+    address: {
+      city: "Dhaka",
+      zip: 1207
+    }
+  };
+
+//   Get name and city from the object using destructuring
+
+//   console.log("Name : ",user.name , "Age : ",  user.age);
+
+//    Rename zip to postalCode
+
+const updateCityAndZip = {
+    ...user,
+   address :  {
+    city : user?.address.city,
+    zip : 3000
+   }
+}
+
+// console.log(updateCityAndZip);
+
+const users = [
+    { name: "Shukumar", address: { city: "Dhaka", zip: 1207 } },
+    { name: "Rita", address: { city: "Khulna", zip: 1612 } }
+  ];
+
+
+const updateUserInfo = users?.map(user=>{
+    if(user?.name === "Rita"){
+        return{
+            ...user,
+            address : {
+                city : user?.address.city,
+                zip : 300.
+            }
+        }
+
+    }
+    return user
+})
+
+console.log(updateUserInfo);
