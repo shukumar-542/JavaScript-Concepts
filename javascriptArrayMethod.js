76aa52f488f0496eeaf1eecca1b1a084eb70f38f
shukumar-542 <shukumar542@gmail.com>
2025-05-22 14:55:28 +0600

    Practice reduce method

--- javascriptArrayMethod.js
+++ javascriptArrayMethod.js
@@ -2,6 +2,8 @@ const celsiusTemps = [0, 10, 20, 30];
 
 // F = C × 1.8 + 32
 
+
+
 const fahrenheit = celsiusTemps.map(celsius =>  (celsius * 1.8 + 32))
 
 // console.log(fahrenheit);
@@ -13,4 +15,22 @@ const upperCase = words.map(name => {
 
 })
 
-console.log(upperCase);
+// Filter Even Number
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenNumber = numbers.filter(number=>  number % 2 === 0)
+// console.log(evenNumber);
+
+
+// Reduce sum
+const nums = [10, 20, 30, 40];
+const totalSum  = nums.reduce((acc, current)=>acc + current ,0)
+
+// console.log(totalSum);
+
+// Flatten an Array of Arrays
+
+const nested = [[1, 2], [3, 4], [5, 6]];
+
+const flatArray = nested.reduce((acc, current)=>acc.concat(current), [])
+console.log(flatArray);
