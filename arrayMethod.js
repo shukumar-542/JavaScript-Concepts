4d7f355f0bb7d77f024203614cfccb80596fa63d
shukumar-542 <shukumar542@gmail.com>
2025-06-14 19:26:29 +0600

    Sort method

--- arrayMethod.js
+++ arrayMethod.js
@@ -1,3 +1,4 @@
+
 const numbers = [10, 22, 45, 60, 75];
 
 const getter = numbers.find(number =>  number > 50);
@@ -26,4 +27,14 @@ const products = [
 
 // ❓ Use .findIndex() to get the index of the product named "Phone".
 const phoneIndex =  products.findIndex(prodcut => prodcut.name === "Phone");
-console.log(phoneIndex)
+// console.log(phoneIndex)
+
+const user = [
+  { name: "Shukumar", age: 25 },
+  { name: "Rita", age: 21 },
+  { name: "Abir", age: 27 }
+];
+
+const marks = [40, 90, 70, 20, 100];
+const sortedUser  = marks.sort((a , b)=> a - b)
+console.log(sortedUser)
