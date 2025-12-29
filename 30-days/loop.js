81a06e4136f4aece6a3fa1aca998842795c11352
shukumar-542 <shukumar542@gmail.com>
2025-12-29 17:02:12 +0600

    Loop excelence

--- 30-days/loop.js
+++ 30-days/loop.js
@@ -5,17 +5,46 @@
 // Task - 1
 
 for (let i = 1; i <= 10; i++) {
+  // console.log(i);
+}
+
+// Task - 2
+
+
+for (let i = 1; i <= 20; i++) {
+
+  if (i % 2 === 0) {
     // console.log(i);
   }
+}
 
-  // Task - 2
+// Task - 3 Print numbers in reverse (10 → 1)
 
 
-  for(let i = 1 ; i <=20; i++){
+for (let i = 10; i >= 1; i--) {
 
-    if(i % 2 === 0){
-        console.log(i);
-    }
+  // console.log(i)
+}
+
+
+// Task 4: Multiplication Table
+
+for (let i = 1; i <= 10; i++) {
+
+  // console.log( `5 * ${i}  = ` ,5 * i)
+}
+
+
+// Task 5: Skip number 5
+
+
+for (let i = 1; i <= 10; i++) {
+
+  if(i === 5){
+    continue;
   }
+  
 
-  // Task - 3
+  console.log( i)
+}
+
